76706a4150e8b848e092fe027104034e8c18a2ca
Trey <Trey@temps-MacBook-Air-2.local>
2019-05-08 15:15:43 -0700

    Redux Additions Complete

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -29,18 +29,48 @@ const removeExpense = ({ id } = {}) => ({
 
 // EDIT_EXPENSE
 
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+});
+
 
 /************************************************************************* */
 
 
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+});
+
 // SORT_BY_DATE
+const sortByDate = () => ({
+  type: 'SORT_BY_DATE',
+});
+
 // SORT_BY_AMOUNT
+const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT',
+
+});
+
 // SET_START_DATE
+const setStartDate = (newStartDate) => ({
+  type: 'SET_START_DATE',
+  newStartDate
+});
+
 // SET_END_DATE
+const setEndDate = (newEndDate) => ({
+  type: 'SET_END_DATE',
+  newEndDate
+});
+
+
 
 // Expenses Reducer
-
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
@@ -52,6 +82,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
       ];
     case 'REMOVE_EXPENSE':
       return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if(expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          }; 
+        } else {
+          return expense;
+        };
+      });
     default:
     return state;
   }
@@ -68,10 +109,59 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch(action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      };
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy: 'date'
+      };
+    case 'SORT_BY_AMOUNT':
+      return {
+        ...state,
+        sortBy: 'amount'
+      };
+    case 'SET_START_DATE':
+      return {
+        ...state,
+        startDate: action.newStartDate
+      };
+    case 'SET_END_DATE':
+      return {
+        ...state,
+        endDate: action.newEndDate
+      };
     default:
     return state;
   }
 };
+
+// timestamps (milliseconds)
+// January 1st 1970 (unix epoch)
+// 33400, 10, -203
+
+
+
+// Get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate}) => {
+  return expenses.filter((expense) => {
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if  (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1;
+    }
+  });
+};
+
 
 // Store creation
 
@@ -79,20 +169,31 @@ const store = createStore(
   combineReducers({
     expenses: expensesReducer,
     filters: filtersReducer
-    
   })
 );
 
 store.subscribe(() => {
-  console.log(store.getState());
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
 });
 
-const expense1 = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
-const expense2 = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+ const expense1 = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+ const expense2 = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -10000 }));
 
-store.dispatch(removeExpense({ id: expense1.expense.id }));
+// store.dispatch(removeExpense({ id: expense1.expense.id }));
+// store.dispatch(editExpense(expense2.expense.id, { amount: 500 }));
 
-console.log(expense1, expense2);
+store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter());
+
+// store.dispatch(sortByAmount());  // amount
+// store.dispatch(sortByDate());  // date
+
+// store.dispatch(setStartDate(0));
+// store.dispatch(setStartDate());
+// store.dispatch(setEndDate(999));
+
 
 const demoState = {
   expenses: [{
@@ -109,3 +210,13 @@ const demoState = {
     endDate: undefined
   }
 };
+
+const user = {
+  name: 'Jen',
+  age: 24
+};
+console.log({
+  age: 27,
+  ...user,
+  location: 'Phili',
+});
---
+++ src/readme.md
@@ -0,0 +1,7 @@
+# Git Commands
+
+git init - create a new git repo
+git status - view the changes to your project
+git add - add files to staging area
+git commit - (-m "") creates a new commit with files from staging area
+git log - view recent commits
